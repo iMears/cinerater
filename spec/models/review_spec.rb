3bd2188b415e2c6314af8ca4e7c1cbe373a60abc
imears <maxwellmears@gmail.com>
2015-05-14 21:18:26 -0700

    Add specs for review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:bad_review) { Review.new }
+  let(:good_review) { Review.new }
+
+  describe "ActiveModel validations" do
+    it { expect(bad_review).to validate_presence_of(:email) }
+    it { expect(bad_review).to validate_presence_of(:created_at) }
+    it { expect(bad_review).to validate_presence_of(:rating) }
+  end
 end
