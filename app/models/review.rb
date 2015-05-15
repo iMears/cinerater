980b33629377b01bda9cef5eb56a81dfe1d6891f
imears <maxwellmears@gmail.com>
2015-05-14 21:23:22 -0700

    Add validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+  validates :email, presence: true
+  validates :created_at, presence: true
+  validates :rating, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  let(:bad_review) { Review.new }
-  let(:good_review) { Review.new }
+  let(:review) { Review.new }
 
   describe "ActiveModel validations" do
-    it { expect(bad_review).to validate_presence_of(:email) }
-    it { expect(bad_review).to validate_presence_of(:created_at) }
-    it { expect(bad_review).to validate_presence_of(:rating) }
+    it { expect(review).to validate_presence_of(:email) }
+    it { expect(review).to validate_presence_of(:created_at) }
+    it { expect(review).to validate_presence_of(:rating) }
   end
 end
